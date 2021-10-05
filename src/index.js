fcb7e8bb4771337f01d7b7ee39c13adca65f3c43
Merly <merly2257@gmail.com>
2021-10-04 21:40:24 -0500

    agregue un poco de java

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+function comprar1(){
+    document.getElementById("pagina1").onclick=function(){document.getElementById("pagina1").style.display="none";
+    document.getElementById("pagina2").style.display="block";
+    }
+}
+
+
+
+
+
+/*
 import validator from './validator.js';
 
 console.log(validator);
