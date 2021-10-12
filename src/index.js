0d0ad26a54af6f30fc52aed2727b00ee06fcc906
Merly <merly2257@gmail.com>
2021-10-11 23:38:37 -0500

    modifique css de la pagina 1

--- src/index.js
+++ src/index.js
@@ -5,19 +5,11 @@ const boxNumber = document.querySelector("#formularioTotal")
 
 //pasar de pagina 1 a pagina 2
 
-document.getElementById("btn2").addEventListener("click", ()=>{
+document.querySelector(".btn1").addEventListener("click", ()=>{
     document.getElementById("pagina1").style.display="none";
-    document.getElementById("pagina2").style.display="block"
+    document.getElementById("pagina3").style.display="block"
 });
 
-//Pasar de pagina 2 a pagina 3
-
-document.getElementById("paso2").addEventListener("click",()=>{
-    document.getElementById("pagina2").style.display="none";
-    document.getElementById("pagina3").style.display="block"
-    ;});
-
-//No dejar que se escriban letras en las caja de numero de tarjeta 
 
 boxNumber.inputNumber.addEventListener("keyup", (e)=>{
     let valorNumero = e.target.value;
