2c1f1c68984945bab8125a1a540261e8e3d5d1b0
Merly <merly2257@gmail.com>
2021-10-26 02:36:35 -0500

    agregue css y js modifique vistas

--- src/index.js
+++ src/index.js
@@ -15,12 +15,12 @@ flechaDerecha.addEventListener("click",()=>{
 });*/
 
 
-//pasar de pagina 1 a pagina 2
+/*pasar de pagina 1 a pagina 2
 
 document.querySelector("#comprar1").addEventListener("click", ()=>{
     document.getElementById("pagina1").style.display="none";
     document.querySelector(".informacion").style.display="block"
-});
+});*/
 
 //PASAR A DATOS RESUMIDOS DE INFORMACIÓN
 let email=document.querySelector("#correoElectronico")
@@ -32,16 +32,73 @@ let distrito = document.querySelector("#distrito")
 const enviarInformacion=document.querySelector("#enviarInformacion")
 
 function enviarInfo(){
-    document.querySelector("#informacion").style.display="none"
-    document.querySelector(".envio").style.display="block"
+    document.querySelector(".informacionDeContacto").style.display="none";
     document.querySelector(".informationTable").style.display="block";
+    document.querySelector(".envio").style.display="block";
     document.querySelector("#mail").innerHTML= email.value;
     document.querySelector("#direccionFinal").innerHTML= direccion.value+", "+distrito.value+", "+provincia.value+ ", "+departamento.value
-    console.log(direccion.value+", "+distrito.value+", "+provincia.value+ ", "+departamento.value)
     //document.querySelector("#direccionFinal").innerHTML= direccion + " " + distrito + " " + provincia + " " + departamento 
 }
 
 enviarInformacion.addEventListener("click",enviarInfo)
+
+//CREANDO CARRITO DE COMPRAS
+
+//------------producto1--------------
+
+document.querySelector("#comprar1").addEventListener("click", ()=>{
+
+    let precio= "S/38.00"
+
+    document.querySelector(".productoF").innerHTML= '<img src="img/product1.jpg" />'
+    document.querySelector(".nombreDelProducto").innerHTML="ACWELL" + "<br>" + "Licorice pH Balancing Advance Serum"
+    document.querySelector(".precioElegido").innerHTML=precio
+    document.querySelector(".envioElegido").innerHTML="Se calculara en el siguiente paso"
+    document.querySelector(".total").innerHTML= precio
+    document.querySelector(".informacionDeContacto").style.display="block";  
+    document.querySelector(".productoElegidos").style.display="block";
+    document.querySelector("#pagina1").style.display="none";
+
+});
+
+//MOSTRAR EL DATO DE ENVIO ELEGIDO
+document.getElementById("Estandar").addEventListener("click",()=>{
+    document.querySelector(".envioElegido").innerHTML="<p>S/10.00</p>";
+    document.querySelector(".total").innerHTML= "S/48.00"
+})
+
+document.getElementById("Recojo").addEventListener("click",()=>{
+    document.querySelector(".envioElegido").innerHTML="<p>S/00.00</p>"
+    document.querySelector(".total").innerHTML= "S/38.00"
+})
+
+//------------------------------producto 2---------------------------
+
+document.querySelector("#comprar2").addEventListener("click", ()=>{
+
+    let precio= "S/80.00"
+
+    document.querySelector(".productoF").innerHTML= '<img src="img/product2.jpg" />'
+    document.querySelector(".nombreDelProducto").innerHTML="SATURDAY SKIN" + "<br>" + "Mascarilla para dormir con vitamina C de Yuzu"
+    document.querySelector(".precioElegido").innerHTML=precio
+    document.querySelector(".envioElegido").innerHTML="Se calculara en el siguiente paso"
+    document.querySelector(".total").innerHTML= precio
+    document.querySelector(".informacionDeContacto").style.display="block";  
+    document.querySelector(".productoElegidos").style.display="block";
+    document.querySelector("#pagina1").style.display="none";
+
+});
+
+//MOSTRAR EL DATO DE ENVIO ELEGIDO
+document.getElementById("Estandar").addEventListener("click",()=>{
+    document.querySelector(".envioElegido").innerHTML="<p>S/10.00</p>";
+    document.querySelector(".total").innerHTML= "S/90.00"
+})
+
+document.getElementById("Recojo").addEventListener("click",()=>{
+    document.querySelector(".envioElegido").innerHTML="<p>S/00.00</p>"
+    document.querySelector(".total").innerHTML= "S/80.00"
+})
 
 //PASAR A LA PAGINA DE PAGO
 const continuarPago = document.querySelector("#continuarPago");
@@ -89,14 +146,52 @@ boxNumber.cvv.addEventListener("keyup", (e)=>{
     .replace(/\D/g, "")
     
 });
+//IDENTIFICAR EL TIPO DE TARJETA INGRESADO
 
+boxNumber.inputNumber.addEventListener("keyup",()=>{
+
+    let valorNumero = document.getElementById("inputNumber").value;
+    
+
+    if (valorNumero.charAt(0)==4){
+        
+        document.querySelector(".tipoDeTarjeta").innerHTML= '<img src="img/visa.png"/>'
+    }
+
+    else if(valorNumero.charAt(0)==5){
+        document.querySelector(".tipoDeTarjeta").innerHTML= '<img src="img/mastercard.png"/>'
+    }
+
+    else if(valorNumero.charAt(0)==6){
+        document.querySelector(".tipoDeTarjeta").innerHTML= '<img src="img/dinerClub.png"/>'
+    }
+
+    else if(valorNumero==""){
+        document.querySelector(".tipoDeTarjeta").innerHTML= " "
+    }
+    else{
+        document.querySelector(".tipoDeTarjeta").innerHTML="La tarjeta ingresada no pertenece a un banco"
+        document.querySelector(".tipoDeTarjeta").style.color="#FF3333"
+    }
+}
+)
 
 //conseguir elementos para validar la tarjeta
+
+
 const creditNumberCard = document.querySelector("#inputNumber");
 
 const btnPagar = document.querySelector("#btnDatos")
 
 function validar(){
+
+    //datos para el resultado final
+
+    let name1=document.querySelector("#name1")
+    let apellido=document.querySelector("#apellidos")
+    let telefono=document.querySelector("#telefono")
+    
+
     let numeroTarjeta = creditNumberCard.value.replace(/\s/g, "");
     let cifrado = validator.maskify(numeroTarjeta)
     //let tipoTarjeta = validator.tarjetType(numeroTarjeta)
@@ -109,8 +204,13 @@ function validar(){
     else if (validator.isValid(numeroTarjeta)){
         document.getElementById("maskifyTarjeta").innerHTML = "La tarjeta que termina en " + cifrado +" es válida";
         document.getElementById("pagina3").style.display="none";
-        document.getElementById("informacion").style.display="none";
-        document.getElementById("resultado").style.display="block";
+        document.getElementById("resultado").style.display="block"
+        document.querySelector(".name2").innerHTM=name1.value
+        document.querySelector(".resumen-nombre").innerHTML=name1.value+" "+apellido.value;
+        document.querySelector(".resumen-correo").innerHTML=email.value;
+        document.querySelector(".resumen-telefono").innerHTML=telefono.value;
+        document.querySelector(".resumen-direccion").innerHTML=direccion.value+", "+distrito.value+", "+provincia.value+ ", "+departamento.value;
+
         /*
         document.getElementById("logo").innerHTML=tipoTarjeta;
         document.getElementById("maskifyTarjeta").innerHTML = "La tarjeta que termina en " + cifrado;
